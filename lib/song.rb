c235246e4a2fe71f5ac9d2f7ba4cdba01a294f4e
michael thomas <michael.thomas2014@gmail.com>
2020-05-14 14:57:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,30 @@
 class Song
-  attr_accessorr :name, :genre, :artist
+  attr_reader :name, :genre, :artist
+  @@count = 0 
+  @@artists = []
+  @@genres = []
+  
+  def self.count
+    @@count
+  end
+  
+  def self.artists
+    @@artists
+  end
+  
+  def self.genres
+    @@genres
+  end
+  
+  def self.genre_count
+    genre_counts = {}
+  end
+  
+  def initialize(name, genre, artist)
+    @name = name
+    @genre = genre
+    @artist = artist
+  end
+    
   
   
